504584da0c259e7d09fe6bef95ad96800c0c1a0e
harkirat <harkriatsingh.tu@gmail.com>
2023-09-07 23:40:17 -0400

    Improved Frontend

--- client/bounce_web/src/App.jsx
+++ client/bounce_web/src/App.jsx
@@ -1,29 +1,82 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MapContainer from './MapContainer'
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 
 import { Wrapper, Status, Spinner, ErrorComponent } from "@googlemaps/react-wrapper";
 
-const center = { lat: 43.6811345, lng: -79.58786719999999 };
-const zoom = 11;
-const render = (status) => {
-  switch (status) {
-    case Status.LOADING:
-      return <h1>Wait....</h1>;
-    case Status.FAILURE:
-      return <h1>Wait....</h1>;
-    case Status.SUCCESS:
-      return <MapContainer center={center} zoom={zoom} />;
-  }
-};
+
 
 function App() {
+  const [routingdata, setRoutingdata] = useState('xxx')
 
   return (
-    <div className='map_main'>
-      <Wrapper apiKey={"AIzaSyANu4rP79yzZDjyHT3ExDgGb_6gh9IxbwE"} render={render} >
-      </Wrapper>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/map' element={<MapComponent props={routingdata} />}></Route>
+        <Route path='/find' element={<AddLocations props={setRoutingdata} />}></Route>
+      </Routes>
 
+    </Router>
+
+  )
+}
+
+
+const MapComponent = ({ props }) => {
+  const center = { lat: 43.6811345, lng: -79.58786719999999 };
+  const zoom = 11;
+  const render = (status) => {
+    switch (status) {
+      case Status.LOADING:
+        return <h1>Wait....</h1>;
+      case Status.FAILURE:
+        return <h1>Wait....</h1>;
+      case Status.SUCCESS:
+        return <MapContainer center={center} zoom={zoom} mapData={props} />;
+    }
+  };
+  return (
+    <div>
+      <div className='map_main'>
+        <Wrapper apiKey={"AIzaSyANu4rP79yzZDjyHT3ExDgGb_6gh9IxbwE"} render={render} >
+        </Wrapper>
+      </div>
+    </div>
+  )
+}
+const AddLocations = ({ props }) => {
+  const [locations, setLocations] = useState(["Bounce 123 Galaxy Boulevard Etobicoke", "40 Barrington Crescent Brampton", "48 Lanark Circle Brampton", "52 Woolgar Avenue Etobicoke", "17 Nagel Road North York", "611 Vaughan Rd Toronto", "83 Avondale Avenue North York", "2864 keele street Toronto", "19 Blaney Crescent North York", "3903 Oland Drive Mississauga", "4149 Chadburn crescent Mississaga", "3 Scarlettwood Court Etobicoke", "10 Knightsbridge Road Brampton", "47 Rowse Cres Etobicoke", "2700 Eglinton ave w Toronto", "25 Colonel Bertram Road Brampton", "134 rotondo cres Kleinburg", "2 Fernwood Rd Toronto"]);
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+
+    const response = await fetch('http://127.0.0.1:8000/api/routing/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ locations })
+    });
+    // Todo: Create a type for the response that you get back from the server
+    const data = await response.json();
+
+
+    if (data) {
+      props(data)
+      navigate("/map");
+    } else {
+      alert("Error");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Assign Locations</h2>
+        <input type='textarea' value={locations} onChange={(e) => setLocations(e.target.value)} placeholder='Locations' />
+        <button onClick={handleClick}>Find</button>
+      </div>
+    </div>
   )
 }
 
--- client/bounce_web/src/MapContainer.jsx
+++ client/bounce_web/src/MapContainer.jsx
@@ -4,145 +4,18 @@ import { React, useEffect, useRef } from 'react'
 
 const locations = ["Bounce 123 Galaxy Boulevard Etobicoke", "40 Barrington Crescent Brampton", "48 Lanark Circle Brampton", "52 Woolgar Avenue Etobicoke", "17 Nagel Road North York", "611 Vaughan Rd Toronto", "83 Avondale Avenue North York", "2864 keele street Toronto", "19 Blaney Crescent North York", "3903 Oland Drive Mississauga", "4149 Chadburn crescent Mississaga", "3 Scarlettwood Court Etobicoke", "10 Knightsbridge Road Brampton", "47 Rowse Cres Etobicoke", "2700 Eglinton ave w Toronto", "25 Colonel Bertram Road Brampton", "134 rotondo cres Kleinburg", "2 Fernwood Rd Toronto"]
 
-const mapData = {
-    "result": {
-        "0": {
-            "path": [
-                0,
-                12,
-                1,
-                15,
-                16,
-                0
-            ],
-            "Total Distance in meters": 77022
-        },
-        "1": {
-            "path": [
-                0,
-                2,
-                9,
-                10,
-                3,
-                0
-            ],
-            "Total Distance in meters": 91897
-        },
-        "2": {
-            "path": [
-                0,
-                8,
-                4,
-                7,
-                6,
-                0
-            ],
-            "Total Distance in meters": 52031
-        },
-        "3": {
-            "path": [
-                0,
-                14,
-                17,
-                5,
-                11,
-                0
-            ],
-            "Total Distance in meters": 36405
-        },
-        "4": {
-            "path": [
-                0,
-                13,
-                0
-            ],
-            "Total Distance in meters": 13071
-        }
-    },
-    "locationsLatLong": [
-        [
-            43.6811345,
-            -79.58786719999999
-        ],
-        [
-            43.7331442,
-            -79.7901081
-        ],
-        [
-            43.6698073,
-            -79.82452409999999
-        ],
-        [
-            43.6254419,
-            -79.5213502
-        ],
-        [
-            43.7281104,
-            -79.50652629999999
-        ],
-        [
-            43.6923105,
-            -79.4398415
-        ],
-        [
-            43.75906,
-            -79.4055245
-        ],
-        [
-            43.7321029,
-            -79.48387869999999
-        ],
-        [
-            43.75448249999999,
-            -79.5219838
-        ],
-        [
-            43.5471904,
-            -79.7442815
-        ],
-        [
-            43.538633,
-            -79.7047171
-        ],
-        [
-            43.69285989999999,
-            -79.51591049999999
-        ],
-        [
-            43.71967859999999,
-            -79.71652809999999
-        ],
-        [
-            43.6913426,
-            -79.5435483
-        ],
-        [
-            43.6899032,
-            -79.478518
-        ],
-        [
-            43.7292381,
-            -79.8164242
-        ],
-        [
-            43.83689080000001,
-            -79.6534341
-        ],
-        [
-            43.70358419999999,
-            -79.4359479
-        ]
-    ]
-}
+
 
 const MapContainer = ({
     center,
     zoom,
+    mapData
 }) => {
 
     const ref = useRef();
 
     useEffect(() => {
+        console.log(mapData)
         let strokeColors = ["#FF0000", "#68FF33", "#33D7FF", "#CA33FF", "#FF33AC", "#5B4B7F", "#E2E25E", "#07F6F6"]
         let locationsLatLong = mapData['locationsLatLong']
 
@@ -177,7 +50,6 @@ const MapContainer = ({
                 path[j] = { lat: locationsLatLong[mapData["result"][i].path[j]][0], lng: locationsLatLong[mapData["result"][i].path[j]][1] }
                 let infowindow = new window.google.maps.InfoWindow({
                     content: locations[mapData["result"][i].path[j]],
-                    ariaLabel: "Bounce123",
                 });
 
                 let deliveryLocation = new window.google.maps.Marker({
@@ -186,7 +58,7 @@ const MapContainer = ({
                 });
                 deliveryLocation.setMap(map)
 
-                infowindow.addListener("click", () => {
+                deliveryLocation.addListener("click", () => {
                     infowindow.open({
                         anchor: deliveryLocation,
                         map,
